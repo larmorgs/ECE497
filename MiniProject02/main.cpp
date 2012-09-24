41da271f74e845988575c4f77079c36aa454ede5
Greg Larmore <larmorgs@rose-hulman.edu>
2012-09-23 23:31:52 -0400

    Adding icons!

--- MiniProject02/main.cpp
+++ MiniProject02/main.cpp
@@ -1,6 +1,7 @@
 #include "../Resources/adafruit/HT1632/HT1632.h"
 #include <stdio.h>
 #include <unistd.h>
+#include "icons.c"
 
 /* 
 This is a basic demo program showing how to write to a HT1632
@@ -43,6 +44,33 @@ void testMatrix(HT1632LEDMatrix *matrix) {
   matrix->blink(false);
 }
 
+void testMatrix2(HT1632LEDMatrix *matrix) {
+  //Display icon
+  matrix->drawBitmap(0, 0, icon1, matrix->width(), matrix->height(), 1);  
+  matrix->writeScreen();
+  usleep(2000000);
+
+  // Blink!
+  matrix->blink(true);
+  usleep(2000000);
+  matrix->blink(false);
+
+  // Dim down and then up
+  for (int8_t i = 15; i >= 0; i--) {
+    matrix->setBrightness(i);
+    usleep(100000);
+  }
+  for (int8_t i = 0; i < 16; i++) {
+    matrix->setBrightness(i);
+    usleep(100000);
+  }
+
+  // Blink again!
+  matrix->blink(true);
+  usleep(2000000);
+  matrix->blink(false);
+}
+
 int main(void) {
   printf("Starting...\n");
   HT1632LEDMatrix matrix = HT1632LEDMatrix(DATA, WR, CS);
@@ -52,7 +80,10 @@ int main(void) {
   printf("Clear\n");
   matrix.clearScreen();
   
-  printf("Test\n");
+  printf("Test2\n");
+  testMatrix2(&matrix);
+  
+  printf("Test1\n");
   testMatrix(&matrix);
   
   printf("Done!\n");
